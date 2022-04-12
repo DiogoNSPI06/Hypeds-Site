d612b51d8e1218e2fd997e60a84bfbdab910ec73
Diogo Nunes Spigato Costa <50158377+DiogoNSPI06@users.noreply.github.com>
2022-04-12 01:02:16 +0000

    v1.4.6

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'HYPED | Home' });
+  //res.render('index', { title: 'HYPED | Home' });
+  res.sendFile('/home/runner/hypedscom/html/index.html')
 });
 
 module.exports = router;
